36a52db1a52f125e183918b7217a29d024213c2b
Mike Joseph <jkellymichael@gmail.com>
2023-05-24 22:44:55 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `firsttest`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
